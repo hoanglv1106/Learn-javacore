1a8ee39a0f8960393109feaa8ab58481c4d1a3f2
Hoanglv <hoanglv1106@gmail.com>
2026-03-02 16:05:02 +0700

    fix: update missing code for Generics & Lambda

--- src/week2/Generics_Lambda/Example1.java
+++ src/week2/Generics_Lambda/Example1.java
@@ -1,4 +1,19 @@
 package week2.Generics_Lambda;
-
+//Viết generic method in ra tất cả phần tử của 1 mảng.
 public class Example1 {
+    public static <T> void printarray(T[] arrays){
+        for(T e: arrays ){
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer [] intArray = {1,2,3,4,5,6,7,8,9};
+        String [] StringArray = {"Hello", "World", "Java", "Generics"};
+        System.out.println("In mảng số nguyên: ");
+        printarray(intArray);
+        System.out.println("In mảng chuỗi: ");
+        printarray(StringArray);
+    }
+
 }
--- src/week2/Generics_Lambda/Example2.java
+++ src/week2/Generics_Lambda/Example2.java
@@ -1,4 +1,20 @@
 package week2.Generics_Lambda;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//Dùng lambda để lọc ra các số chẵn trong List<Integer>.
 public class Example2 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> evenNumbers = numbers.stream().filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+        ;
+        System.out.println("các số chẵn trong danh sách là:");
+        System.out.println(evenNumbers);
+    }
 }
+
+
+
